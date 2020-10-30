3300b65c81d915533239a91ecc97bf6d4c3298f3
ljf8838 <ljf8838@163.com>
2020-10-30 11:06:16 +0800

    Initial commit

---
+++ src/main/java/com/lydsj/CustomerpicApplication.java
@@ -0,0 +1,13 @@
+package com.lydsj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CustomerpicApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CustomerpicApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/lydsj/CustomerpicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lydsj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerpicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
